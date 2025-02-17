1cb3ea44a52f8dd80c8069c116075881653caba7
danielsalgador <danielsalgador@gmail.com>
2025-02-17 18:56:37 -0500

    Comandos Git

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         System.out.println("Comandos Git");
+
+        System.out.println("Comands Git");
+        System.out.println("git status");
+        System.out.println("git add .");
+        System.out.println("git commit -m");
+        System.out.println("git push origin nombrerama");
+        System.out.println("git branch");
+        System.out.println("git switch");
+
     }
 }
